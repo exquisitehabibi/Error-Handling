4ec4648fd88be9cf75f96d28070dcbc5b5fbed6f
Arun Thakur <120034666+exquisitehabibi@users.noreply.github.com>
2024-06-04 16:15:05 +0530

    Update Error Handling.sol

--- Error Handling.sol
+++ Error Handling.sol
@@ -2,21 +2,22 @@
 pragma solidity ^0.8.13;
 
 contract ErrorHandling {
+    
     mapping(address => uint) public balance;
     address owner = msg.sender; 
     /*the owner address is set to be as of the sender for easy demonstration of the code.
     It can be changed to the address of account we are currently using.*/
 
     function addtokens(uint amt) public {
-        require(msg.sender == owner, "The transaction must be done by the owner");
-        assert(amt > 0);
+        require(msg.sender == owner, "The transaction must be done by the owner"); // require() statement
+        assert(amt > 0); // assert() statement
         balance[owner] += amt;
     }
 
     function withdraw(uint amt) public {
-        require(msg.sender == owner, "The transaction must be done by the owner");
+        require(msg.sender == owner, "The transaction must be done by the owner"); // require() statement
         if(amt > balance[owner]){
-            revert("The balance is insufficient for transaction");
+            revert("The balance is insufficient for transaction"); // revert() statement
         }
         balance[owner] -= amt;
     }
